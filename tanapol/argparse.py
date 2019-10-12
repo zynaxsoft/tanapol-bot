bbbf6ded521dc6a5d044cd9de7dbd2e12046dd1d
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-10-12 20:15:13 +0900

    use configargparse

--- tanapol/argparse.py
+++ tanapol/argparse.py
@@ -1,7 +1,15 @@
-import argparse
+import os
+
+import tanapol
+import configargparse
 
 
-p = argparse.ArgumentParser()
-p.add_argument('--secrets-file', '-f',
-               help='location of the secrets file')
+config_path = os.path.join(tanapol.ROOT_DIR, 'config.yml')
+p = configargparse.ArgParser(default_config_files=[config_path])
+p.add('-c', '--config', required=False, is_config_file=True,
+      help='custom config file path')
+p.add('--secrets-file', '-f',
+      help='location of the secrets file')
+p.add('--user-id',
+      help='Slack user ID that is given by the slack api')
 args = p.parse_args()
