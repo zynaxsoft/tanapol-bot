0c12d18295627e631d8b71e44f0ea425843dd648
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-10-12 20:51:33 +0900

    add verbosity

--- tanapol/argparse.py
+++ tanapol/argparse.py
@@ -8,6 +8,8 @@ config_path = os.path.join(tanapol.ROOT_DIR, 'config.yml')
 p = configargparse.ArgParser(default_config_files=[config_path])
 p.add('-c', '--config', required=False, is_config_file=True,
       help='custom config file path')
+p.add('-v', action='count', default=0,
+      help='verbosity')
 p.add('--secrets-file', '-f',
       help='location of the secrets file')
 p.add('--user-id',
--- tanapol/log.py
+++ tanapol/log.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+from tanapol.argparse import args
+
 
 logger = logging.getLogger('tanapol')
 log_format = logging.Formatter('[%(levelname)s] %(message)s')
@@ -8,17 +10,11 @@ logger.setLevel(logging.DEBUG)
 
 stream_handler = logging.StreamHandler(stream=sys.stdout)
 stream_handler.setFormatter(log_format)
+verbosity = min(2, args.v)
+stream_handler.setLevel(logging.WARNING - logging.DEBUG * verbosity)
 logger.addHandler(stream_handler)
 
-
-def set_log_level(v):
-    verbosity = 0
-    if v is not None:
-        verbosity = v
-        if verbosity > 2:
-            print('Too much verbosity. Setting verbosity to -vv')
-            verbosity = 2
-    stream_handler.setLevel(logging.WARNING - logging.DEBUG * verbosity)
+logger.debug('test')
 
 
 def handle_exception(exc_type, exc_value, exc_traceback, **kwargs):
