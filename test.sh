898836d23074a967c8775ce02b85df8fb0bbe95f
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-10-13 11:59:37 +0900

    add test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Exit immediately when exit code is not 0
+set -e
+
+PACKAGE=tanapol
+
+# Install linters
+pip3 install flake8 > /dev/null
+pip3 install pylint > /dev/null
+
+flake8 ${PACKAGE} --ignore "I100, E123, W503"
+python3 -m pylint ${PACKAGE} --errors-only
